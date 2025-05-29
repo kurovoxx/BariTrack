7d806a925f6064fcab6f3b1247f70cf1868299a7
JorgeFierroo <135017288+JorgeFierroo@users.noreply.github.com>
2025-05-28 21:36:55 -0400

    Update Memento.py

--- Clases/Memento.py
+++ Clases/Memento.py
@@ -1,1 +1,22 @@
+from Usuario import Usuario
+
+class MementoUsuario:
+    def __init__(self, estado: dict):
+        self._estado = estado.copy()
+
+    def get_estado(self) -> dict:
+        return self._estado.copy()
+
+
+class UsuarioCaretaker:
+    def __init__(self):
+        self._historial = []
+
+    def guardar_memento(self, memento):
+        self._historial.append(memento)
+
+    def deshacer(self):
+        if self._historial:
+            return self._historial.pop()
+        return None
 
